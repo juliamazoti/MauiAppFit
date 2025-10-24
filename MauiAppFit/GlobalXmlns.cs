1b608d39a87a6d8139db80eebcc7ac72c4c19a4e
juliamazoti <juliamazoti.univ@gmail.com>
2025-10-24 19:28:24 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppFit")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppFit.Pages")]
